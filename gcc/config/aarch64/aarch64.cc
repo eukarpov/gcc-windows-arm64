0adc7b9a8428856d84a855411fe1b6b58fa123fc
Evgeny Karpov <eukarpov@gmail.com>
2024-11-15 13:14:18 +0100

    aarch64: Add SEH, stack unwinding and C++ exceptions.
    This patch reuses the existing SEH, stack unwinding and C++ exceptions
    from ix86 and implements the required changes for AArch64.
    
    gcc/ChangeLog:
    
    	* common/config/aarch64/aarch64-common.cc (aarch64_except_unwind_info):
    	Add unwind info for AArch64.
    	(defined): Likewise.
    	(TARGET_EXCEPT_UNWIND_INFO): Likewise.
    	* config/aarch64/aarch64.cc (aarch64_print_reg):
    	Print a reg.
    	(defined): Enable unwind tables.
    	(aarch64_declare_function_name): Add unwinding.
    	* config/aarch64/cygming.h (SYMBOL_REF_STUBVAR_P):
    	Enable SEH, declare required functions and parameters.
    	(TARGET_SEH): Likewise.
    	(SEH_MAX_FRAME_SIZE): Likewise.
    	(TARGET_ASM_UNWIND_EMIT): Likewise.
    	(TARGET_ASM_UNWIND_EMIT_BEFORE_INSN): Likewise.
    	(TARGET_ASM_FUNCTION_END_PROLOGUE): Likewise.
    	(TARGET_ASM_EMIT_EXCEPT_PERSONALITY): Likewise.
    	(TARGET_ASM_INIT_SECTIONS): Likewise.
    	(SUBTARGET_ASM_UNWIND_INIT): Likewise.
    	(aarch64_pe_seh_unwind_emit): Likewise.
    	(aarch64_print_reg): Likewise.
    	(ASM_DECLARE_FUNCTION_SIZE): Likewise.
    	(ASM_DECLARE_COLD_FUNCTION_SIZE): Likewise.
    	(ASM_DECLARE_COLD_FUNCTION_NAME): Likewise.
    	* config/mingw/winnt.cc (defined):
    	Add AArch64 implmentation for SEH.
    	(CALLEE_SAVED_REG_NUMBER): Likewise.
    	(seh_parallel_offset): Likewise.
    	(seh_pattern_emit): Likewise.
    	(aarch64_pe_seh_unwind_emit): Likewise.
    
    libgcc/ChangeLog:
    
    	* config.host: Support AArch64.
    	* unwind-seh.c (defined): Likewise.
    	(_Unwind_Backtrace): Likewise.

--- gcc/config/aarch64/aarch64.cc
+++ gcc/config/aarch64/aarch64.cc
@@ -13198,6 +13198,12 @@ aarch64_label_mentioned_p (rtx x)
   return 0;
 }
 
+void
+aarch64_print_reg (rtx x, int code, FILE *file)
+{
+  aarch64_print_operand (file, x, code);
+}
+
 /* Implement REGNO_REG_CLASS.  */
 
 enum reg_class
@@ -19028,6 +19034,12 @@ aarch64_override_options_after_change_1 (struct gcc_options *opts)
      intermediary step for the former.  */
   if (flag_mlow_precision_sqrt)
     flag_mrecip_low_precision_sqrt = true;
+
+  /* Enable unwind tables for MS.  */
+#if defined (TARGET_AARCH64_MS_ABI)
+  if (opts->x_flag_unwind_tables == 0)
+    opts->x_flag_unwind_tables = 1;
+#endif // TARGET_AARCH64_MS_ABI
 }
 
 /* 'Unpack' up the internal tuning structs and update the options
@@ -25602,6 +25614,9 @@ aarch64_declare_function_name (FILE *stream, const char* name,
 
   /* Don't forget the type directive for ELF.  */
   ASM_OUTPUT_TYPE_DIRECTIVE (stream, name, "function");
+#ifdef SUBTARGET_ASM_UNWIND_INIT
+  SUBTARGET_ASM_UNWIND_INIT (stream);
+#endif
   ASM_OUTPUT_FUNCTION_LABEL (stream, name, fndecl);
 
   cfun->machine->label_is_assembled = true;
