4dd87f8586d19424bc995b4821b697c54a6838da
Evgeny Karpov <eukarpov@gmail.com>
2024-11-15 13:36:41 +0100

    Rename SEH functions for reuse in AArch64
    This patch renames functions related to SEH functionality. These
    functions will be reused in the aarch64-w64-mingw32 target.
    
    gcc/ChangeLog:
    
    	* config/i386/cygming.h (TARGET_ASM_FUNCTION_END_PROLOGUE):
    	Rename.
    	(TARGET_ASM_EMIT_EXCEPT_PERSONALITY): Likewise.
    	(TARGET_ASM_INIT_SECTIONS): Likewise.
    	(SUBTARGET_ASM_UNWIND_INIT): Likewise.
    	(ASM_DECLARE_FUNCTION_SIZE): Likewise.
    	(ASM_DECLARE_COLD_FUNCTION_SIZE): Likewise.
    	* config/i386/i386-protos.h (i386_pe_end_function):
    	Remove declarations.
    	(i386_pe_end_cold_function): Likewise.
    	(i386_pe_seh_init): Likewise.
    	(i386_pe_seh_end_prologue): Likewise.
    	(i386_pe_seh_cold_init): Likewise.
    	(i386_pe_seh_emit_except_personality): Likewise.
    	(i386_pe_seh_init_sections): Likewise.
    	* config/mingw/winnt.cc (i386_pe_seh_init): Rename into ...
    	(mingw_pe_seh_init): ... this.
    	(i386_pe_seh_end_prologue): Rename into ...
    	(mingw_pe_seh_end_prologue): ... this.
    	(i386_pe_seh_cold_init): Rename into ...
    	(mingw_pe_seh_cold_init): ... this.
    	(i386_pe_seh_fini): Rename into ...
    	(mingw_pe_seh_fini): ... this.
    	(i386_pe_seh_emit_except_personality): Rename into ...
    	(mingw_pe_seh_emit_except_personality): ... this.
    	(i386_pe_seh_init_sections): Rename into ...
    	(mingw_pe_seh_init_sections): ... this.
    	(i386_pe_end_function): Rename into ...
    	(mingw_pe_end_function): ... this.
    	(i386_pe_end_cold_function): Rename into ...
    	(mingw_pe_end_cold_function): ... this.
    	* config/mingw/winnt.h (mingw_pe_end_cold_function):
    	Add declarations.
    	(mingw_pe_end_function): Likewise.
    	(mingw_pe_seh_init): Likewise.
    	(mingw_pe_seh_init_sections): Likewise.
    	(mingw_pe_seh_cold_init): Likewise.
    	(mingw_pe_seh_emit_except_personality): Likewise.
    	(mingw_pe_seh_end_prologue): Likewise.
    
    libgcc/ChangeLog:
    
    	* config.host: Update.
    	* config/i386/t-seh-eh: Move to...
    	* config/mingw/t-seh-eh: ...here.

--- gcc/config/mingw/winnt.h
+++ gcc/config/mingw/winnt.h
@@ -27,6 +27,8 @@ extern void mingw_pe_asm_lto_start (void);
 extern void mingw_pe_asm_lto_end (void);
 extern void mingw_pe_declare_type (FILE *, const char *, bool, bool);
 extern void mingw_pe_encode_section_info (tree, rtx, int);
+extern void mingw_pe_end_cold_function (FILE *, const char *, tree);
+extern void mingw_pe_end_function (FILE *, const char *, tree);
 extern void mingw_pe_file_end (void);
 extern void mingw_pe_maybe_record_exported_symbol (tree, const char *, int);
 extern void mingw_pe_record_stub (const char *, bool);
@@ -34,6 +36,12 @@ extern unsigned int mingw_pe_section_type_flags (tree, const char *, int);
 extern section *mingw_pe_select_section (tree, int, unsigned HOST_WIDE_INT);
 extern void mingw_pe_unique_section (tree, int);
 extern bool mingw_pe_valid_dllimport_attribute_p (const_tree);
+
+extern void mingw_pe_seh_init (FILE *);
+extern void mingw_pe_seh_init_sections (void);
+extern void mingw_pe_seh_cold_init (FILE *, const char *);
+extern void mingw_pe_seh_emit_except_personality (rtx);
+extern void mingw_pe_seh_end_prologue (FILE *);
 
 #endif /* not USED_FOR_TARGET.  */
 
