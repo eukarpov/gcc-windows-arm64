9b7d185ab6ca728b48a1f3243d82113e8efe2b60
Evgeny Karpov <eukarpov@gmail.com>
2024-11-15 13:51:42 +0100

    aarch64: Add stdcall and cdecl attributes
    This patch adds stdcall and cdecl attributes, which might be used for
    DLL export/import in MinGW.
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64.cc: Update.

--- gcc/config/aarch64/aarch64.cc
+++ gcc/config/aarch64/aarch64.cc
@@ -875,6 +875,8 @@ static const attribute_spec aarch64_gnu_attributes[] =
   { "Advanced SIMD type", 1, 1, false, true,  false, true,  NULL, NULL },
   { "SVE type",		  3, 3, false, true,  false, true,  NULL, NULL },
   { "SVE sizeless type",  0, 0, false, true,  false, true,  NULL, NULL },
+  { "cdecl",		  0, 0, false, false, false, false, NULL, NULL },
+  { "stdcall",		  0, 0, false, false, false, false, NULL, NULL },
 #if TARGET_DLLIMPORT_DECL_ATTRIBUTES
   { "dllimport", 0, 0, false, false, false, false, handle_dll_attribute, NULL },
   { "dllexport", 0, 0, false, false, false, false, handle_dll_attribute, NULL },
