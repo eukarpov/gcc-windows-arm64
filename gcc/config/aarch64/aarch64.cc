2eb7c50c6b823d8136d6e118df5c7c2934b0c81e
WhatAmISupposedToPutHere <98837604+WhatAmISupposedToPutHere@users.noreply.github.com>
2025-06-13 11:55:40 +0200

    Add support for shared attribute (#41)
    Cygwin uses shared secions, and gcc previously only recognized
    this attribute on x86
    
    Signed-off-by: Sasha Finkelstein <fnkl.kernel@gmail.com>

--- gcc/config/aarch64/aarch64.cc
+++ gcc/config/aarch64/aarch64.cc
@@ -880,6 +880,7 @@ static const attribute_spec aarch64_gnu_attributes[] =
 #if TARGET_DLLIMPORT_DECL_ATTRIBUTES
   { "dllimport", 0, 0, false, false, false, false, handle_dll_attribute, NULL },
   { "dllexport", 0, 0, false, false, false, false, handle_dll_attribute, NULL },
+  { "shared",    0, 0, true,  false, false, false, mingw_handle_shared_attribute, NULL },
 #endif
 #ifdef SUBTARGET_ATTRIBUTE_TABLE
   SUBTARGET_ATTRIBUTE_TABLE
